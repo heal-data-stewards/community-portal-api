d9af6ca8e795e97e36e144223756b0ea12e5e63b
Sergio Rocha <srocha@rciadmins-MacBook-Pro.local>
2021-08-26 08:25:35 -0700

    Configuered a new email plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,26 @@
 module.exports = ({ env }) => ({
   upload: {
-    provider: 'aws-s3',
+    provider: "aws-s3",
     providerOptions: {
-      accessKeyId: env('AWS_ACCESS_KEY_ID'),
-      secretAccessKey: env('AWS_ACCESS_SECRET'),
-      region: env('AWS_REGION'),
+      accessKeyId: env("AWS_ACCESS_KEY_ID"),
+      secretAccessKey: env("AWS_ACCESS_SECRET"),
+      region: env("AWS_REGION"),
       params: {
-        Bucket: env('AWS_BUCKET_NAME'),
+        Bucket: env("AWS_BUCKET_NAME"),
       },
     },
   },
+  email: {
+    provider: "sendmail",
+    providerOptions: {
+      dkim: {
+        privateKey: "../dkim-private.pem",
+        keySelector: "abcd", // the same as the one set in DNS txt record, use online dns lookup tools to be sure that is retreivable
+      },
+    },
+    settings: {
+      defaultFrom: "srocha@renci.org",
+      defaultReplyTo: "srocha@renci.org",
+    },
+  },
 });
-
