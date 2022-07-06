3e3fad9f086ab628ecef9637b39d9e0bac015e2c
Chris Ball <christopherball@rti.org>
2022-07-06 17:28:08 +0000

    Pushing up all current changes

--- config/database.js
+++ config/database.js
@@ -12,7 +12,7 @@ module.exports = ({ env }) => ({
         password: env('DATABASE_PASSWORD', 'Tict@c997!'),
         ssl: env.bool('DATABASE_SSL', false),
       },
-      options: {}
+      options: {autoMigration: true}
     },
   },
 });
